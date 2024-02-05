f6f2e02dd8beda7637eecf852fb6137ac49ccf37
Vengart <112257626+Vengart@users.noreply.github.com>
2024-02-05 15:32:57 +0200

    Delete Control Task directory

--- Control Task/server/routes/comments.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = new Router()
-import {checkAuth} from '../utils/checkAuth.js'
-import { createComment } from "../controllers/comments.js";
-
-router.post('/:id', checkAuth, createComment)
-
-export default router
